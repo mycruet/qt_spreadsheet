d17fd40f93f65325c669be86a537a6b6bee3afbf
wjj <bbs20009cn@163.com>
2016-05-16 13:30:09 +0800

    add images

--- spreadsheet/main.cpp
+++ spreadsheet/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QSplashScreen>
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    
-    return a.exec();
+
+    QApplication app(argc, argv);
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/images/splash.png"));
+    splash->show();
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    splash->showMessage(QObject::tr("Setting up the main window..."),
+                        topRight, Qt::white);
+    MainWindow mainWin;
+    splash->showMessage(QObject::tr("Loading modules..."),
+                        topRight, Qt::white);
+    //loadModules();
+    splash->showMessage(QObject::tr("Establishing connections..."),
+                        topRight, Qt::white);
+   // establishConnections();
+    mainWin.show();
+    splash->finish(&mainWin);
+    delete splash;
+    return app.exec();
+
 }
